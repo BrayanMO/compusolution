1432a81ecaddb4653d1d16764a2385a9c2758e81
brayanMO <brayanmartinezojeda0@gmail.com>
2021-12-03 11:05:32 -0500

    cambios 8.6

--- resources/views/livewire/payment-order.blade.php
+++ resources/views/livewire/payment-order.blade.php
@@ -21,7 +21,7 @@
             $item = new MercadoPago\Item();
             $item->title = $product->name;
             $item->quantity = $product->qty;
-            $item->unit_price = $product->price;
+            $item->unit_price = ($product->price + order->shipping_cost);
 
             $products[] = $item;
         }
